68091dfeb8923c4063e81774282af0d697e7a71c
maiconkkl <ma_bonitao@hotmail.com>
2020-01-06 22:03:35 -0300

    Correcting and improving translation

--- __resource.lua
+++ __resource.lua
@@ -7,6 +7,7 @@ version '1.1.0'
 client_scripts {
 	'@es_extended/locale.lua',
 	'locales/de.lua',
+	'locales/br.lua',
 	'locales/en.lua',
 	'locales/fi.lua',
 	'locales/fr.lua',
@@ -19,6 +20,7 @@ client_scripts {
 server_scripts {
 	'@es_extended/locale.lua',
 	'locales/de.lua',
+	'locales/br.lua',
 	'locales/en.lua',
 	'locales/fi.lua',
 	'locales/fr.lua',
---
+++ locales/br.lua
@@ -0,0 +1,16 @@
+Locales['br'] = {
+  ['shop_robbery'] = 'roubo de loja',
+  ['press_to_rob'] = 'pressione ~INPUT_CONTEXT~ para ~o~roubar~s~ ~b~%s~s~',
+  ['robbery_timer'] = 'assalto à loja: ~r~%s~s~ segundo(s) restante(s)',
+  ['recently_robbed'] = 'esta loja foi recentemente roubada. Aguarde ~y~%s~s~ segundo(s) até que você possa roubar novamente',
+  ['rob_in_prog'] = '~r~roubo em andamento em ~b~%s~s~',
+  ['started_to_rob'] = 'você começou a roubar ~y~%s~s~',
+  ['alarm_triggered'] = 'o alarme foi acionado',
+  ['robbery_complete'] = '~r~O assalto foi bem sucedido~s~, você ~o~roubou~s~ ~g~$%s~s~',
+  ['robbery_complete_at'] = '~r~Assalto bem sucedido em ~y~%s~s~',
+  ['robbery_cancelled'] = 'o assalto foi cancelado!',
+  ['robbery_cancelled_at'] = '~r~O assalto em ~b~%s~s~ foi cancelado!',
+  ['min_police'] = 'deve haver pelo menos ~b~%s policiais~s~ na cidade para roubar.',
+  ['robbery_already'] = '~r~Um assalto já está em andamento.',
+  ['no_threat'] = 'você não representa uma ameaça para o lojista',
+}
